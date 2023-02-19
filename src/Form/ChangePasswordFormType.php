0c42b65c308720b52efaaece7c43a7b7af30be96
meganesmaili <megane.smaili.dev@gmail.com>
2023-02-19 10:52:43 +0100

    dernière_modif_reset_pass_lien vers mail

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -30,13 +30,13 @@ class ChangePasswordFormType extends AbstractType
                             'max' => 4096,
                         ]),
                     ],
-                    'label' => 'New password',
+                    'label' => 'Nouveau mot de passe',
                 ],
                 'second_options' => [
                     'attr' => ['autocomplete' => 'new-password'],
-                    'label' => 'Repeat Password',
+                    'label' => 'Répéter le mot de passe',
                 ],
-                'invalid_message' => 'The password fields must match.',
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
